4e3dc1f7b832028780995cf8a36d81b3701a5e4a
dioripribadisinaga@gmail.com <dioripribadisinaga@gmail.com>
2022-09-30 01:39:02 +0700

    perubahan, dan Create Readme

--- app/controller/api/v1/reflectionController.js
+++ app/controller/api/v1/reflectionController.js
@@ -16,8 +16,7 @@ const createReflection=async (req,res)=>{
         const { rowCount, rows } = await Reflection.FindAll()
         const id = rowCount === 0 ? 1 : (rows[rows.length - 1].id + 1)
         const {success,low_point,take_away}=req.body
-        const created_date=new Date().toLocaleString("id-ID")
-        const result=await Reflection.CreateReflection(id,success,low_point,take_away,owner_id,created_date,created_date)
+        const result=await Reflection.CreateReflection(id,success,low_point,take_away,owner_id)
         res.status(201).json(result.rows[0])
     }catch (e){
         res.status(500).json(e)
